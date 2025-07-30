757fdf6a6249334461a858b71e04b89fa7f161c9
Lucas Walter <wsacul@gmail.com>
2025-07-30 14:36:28 -0700

    use postcard to encode a struct, next decode it in the receiver

--- net_loopback/src/bin/node0.rs
+++ net_loopback/src/bin/node0.rs
@@ -1,7 +1,7 @@
 /*!
 Send packets to a receiver
 
-recieve on command line:
+receive on command line:
 
 ```
 netcat -ul 34201
@@ -15,7 +15,17 @@ netcat -ul 34201 | hexdump -C
 
 */
 
+use postcard::{from_bytes_crc32, to_stdvec_crc32};
+use serde::{Deserialize, Serialize};
 use std::net::UdpSocket;
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+struct SomeData {
+    counter: u64,
+    value0: f64,
+    value1: u32,
+    value2: u8,
+}
 
 fn main() -> std::io::Result<()> {
     let addr0 = "127.0.0.1:34200";
@@ -23,12 +33,25 @@ fn main() -> std::io::Result<()> {
     let addr1 = "127.0.0.1:34201";
     println!("{socket:?}");
 
-    let mut buf = [0x33, 0xBE, 0x0, 0x4, 0x6, 0x9];
+    let crc = crc::Crc::<u32>::new(&crc::CRC_32_ISCSI);
+    // let mut msg_bytes = [0x33, 0xBE, 0x0, 0x4, 0x6, 0x9];
+    let mut data = SomeData::default();
+
     loop {
-        buf[2] += 1;
-        let rv = socket.send_to(&buf, &addr1);
-        println!("sent {buf:X?}, {rv:?}");
         std::thread::sleep(std::time::Duration::from_secs(1));
+        let msg_bytes = {
+            match to_stdvec_crc32(&data, crc.digest()) {
+                Ok(msg_bytes) => msg_bytes,
+                Err(err) => {
+                    eprintln!("{err:?}");
+                    continue;
+                }
+            }
+        };
+        let rv = socket.send_to(&msg_bytes, &addr1);
+        println!("sent {data:?} encoded as {msg_bytes:X?}, rv {rv:?}");
+        // msg_bytes[2] += 1;
+        data.counter += 1;
     }
 
     // Ok(())
