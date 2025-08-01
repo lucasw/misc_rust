346da7687769039abc690e87767ca5d5f595c087
Lucas Walter <wsacul@gmail.com>
2025-08-01 08:49:41 -0700

    forgot to add lib and logger files before, also have structs that will be used with postcard

---
+++ nucleo_postcard/src/lib.rs
@@ -0,0 +1,18 @@
+#![no_std]
+
+use serde::{Deserialize, Serialize};
+
+pub mod logger;
+
+#[derive(Clone, Serialize, Deserialize, Debug, Default)]
+pub struct SomeData {
+    pub counter: u64,
+    pub value0: f64,
+    pub value1: u32,
+    pub value2: u8,
+}
+
+#[derive(Clone, Serialize, Deserialize, Debug, Default)]
+pub struct SmallArray {
+    pub data: [u8; 32],
+}
