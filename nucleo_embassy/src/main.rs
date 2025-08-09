b53ac25c87f9bad6df2c84728ae1c7bb456d2526
Lucas Walter <wsacul@gmail.com>
2025-08-08 18:11:42 -0700

    now have udp messages getting sent from embassy

--- nucleo_embassy/src/main.rs
+++ nucleo_embassy/src/main.rs
@@ -1,3 +1,6 @@
+/*!
+nc -ul 34200 | hexdump
+*/
 #![no_main]
 #![no_std]
 
@@ -6,11 +9,33 @@ use embassy_executor::{Spawner, main, task};
 use embassy_stm32::gpio::{Level, Output, Speed};
 use embassy_time::Timer;
 
+use embassy_net::udp::{PacketMetadata, UdpMetadata, UdpSocket};
+use embassy_net::{Ipv4Address, Ipv4Cidr, StackResources};
+use embassy_stm32::eth::{Ethernet, GenericPhy, PacketQueue};
+use embassy_stm32::peripherals::ETH;
+use embassy_stm32::{bind_interrupts, eth, peripherals, rng};
+
+// use smoltcp::socket::udp::UdpMetadata};
+use smoltcp::wire::{IpAddress, IpEndpoint};
+use static_cell::StaticCell;
+
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
     hprintln!("{}", info);
     loop {}
 }
+
+type Device = Ethernet<'static, ETH, GenericPhy>;
+
+#[embassy_executor::task]
+async fn net_task(mut runner: embassy_net::Runner<'static, Device>) -> ! {
+    runner.run().await
+}
+
+bind_interrupts!(struct Irqs {
+    ETH => eth::InterruptHandler;
+    HASH_RNG => rng::InterruptHandler<peripherals::RNG>;
+});
 
 #[main]
 async fn main(spawner: Spawner) {
@@ -26,9 +51,86 @@ async fn main(spawner: Spawner) {
     let led_orange = Output::new(p.PE1, Level::Low, Speed::Medium);
     let led_red = Output::new(p.PB14, Level::High, Speed::Medium);
 
+    // embassy/examples/stm32h7/src/bin/eth.rs
+    let mac_addr = [0x00, 0x00, 0xDE, 0xAD, 0xBE, 0xEF];
+    static PACKETS: StaticCell<PacketQueue<4, 4>> = StaticCell::new();
+
+    let device = Ethernet::new(
+        PACKETS.init(PacketQueue::<4, 4>::new()),
+        p.ETH,
+        Irqs,
+        // these match nucleo-h7xx/src/ethernet.rs Pins
+        p.PA1,  // ref_clk
+        p.PA2,  // mdio
+        p.PC1,  // eth_mdc
+        p.PA7,  // CRS_DV: Carrier Sense
+        p.PC4,  // RX_D0: Received Bit 0
+        p.PC5,  // RX_D1: Received Bit 1
+        p.PG13, // TX_D0: Transmit Bit 0
+        p.PB13, // TX_D1: Transmit Bit 1
+        p.PG11, // TX_EN: Transmit Enable
+        GenericPhy::new_auto(),
+        mac_addr,
+    );
+
+    // Generate random seed
+    let mut rng = rng::Rng::new(p.RNG, Irqs);
+    let mut seed = [0; 8];
+    rng.fill_bytes(&mut seed);
+    let seed = u64::from_le_bytes(seed);
+
+    // from_bytes is in smoltcp, but not exposed through embassy_net?
+    // let local_ip_addr = Ipv4Address::from_bytes(&[192, 168, 0, 123]);
+    let local_ip_addr = Ipv4Address::new(192, 168, 0, 123);
+
+    let config = embassy_net::Config::ipv4_static(embassy_net::StaticConfigV4 {
+        address: Ipv4Cidr::new(local_ip_addr, 24),
+        dns_servers: heapless::Vec::new(),
+        gateway: Some(Ipv4Address::new(192, 168, 0, 1)),
+    });
+
+    // Init network stack
+    static RESOURCES: StaticCell<StackResources<3>> = StaticCell::new();
+    let (stack, runner) =
+        embassy_net::new(device, config, RESOURCES.init(StackResources::new()), seed);
+    spawner.spawn(net_task(runner)).unwrap();
+
+    let mut rx_meta = [PacketMetadata::EMPTY; 16];
+    let mut rx_buffer = [0; 4096];
+    let mut tx_meta = [PacketMetadata::EMPTY; 16];
+    let mut tx_buffer = [0; 4096];
+    let mut buf = [0; 4096];
+
+    let mut socket = UdpSocket::new(
+        stack,
+        &mut rx_meta,
+        &mut rx_buffer,
+        &mut tx_meta,
+        &mut tx_buffer,
+    );
+    socket.bind(34201).unwrap();
+
+    let endpoint = UdpMetadata {
+        // TODO(lucasw) use build.rs to set target ip address, also experiment with broadcast
+        // 255 has the same behavior as with nucleo-h7xx- one packet is received then no more
+        endpoint: IpEndpoint::new(Ipv4Address::new(192, 168, 0, 100).into(), 34200),
+        local_address: Some(IpAddress::Ipv4(local_ip_addr)),
+        meta: smoltcp::phy::PacketMeta::default(),
+    };
+
     spawner.must_spawn(flash_led(led_green, 500));
     spawner.must_spawn(flash_led(led_orange, 1100));
     spawner.must_spawn(flash_led(led_red, 2250));
+
+    let mut count = 0;
+    loop {
+        buf[0] = 0xff;
+        buf[1] = count as u8;
+        buf[2] = (count / 256) as u8;
+        socket.send_to(&buf[0..16], endpoint).await.unwrap();
+        Timer::after_millis(200).await;
+        count += 1;
+    }
 }
 
 #[task(pool_size = 3)]
