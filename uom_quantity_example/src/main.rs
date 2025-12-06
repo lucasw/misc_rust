3d846d5746fa42f97784a479bc531d5b106b52ac
Lucas Walter <wsacul@gmail.com>
2025-12-06 15:11:05 -0800

    convenience methods for curvature and turn radius

--- uom_quantity_example/src/main.rs
+++ uom_quantity_example/src/main.rs
@@ -1,6 +1,7 @@
 use typenum::{N1, Z0};
 use uom::{
     Kind,
+    num_traits::Zero,
     si::{
         ISQ, Quantity, SI,
         f64::{Length, V},
@@ -14,12 +15,11 @@ type InverseMeter = Quantity<ISQ<N1, Z0, Z0, Z0, Z0, Z0, Z0, dyn Kind>, SI<V>, V
 // other application of inverse meter, need to wrap it in a struct to enforce that?
 #[derive(Debug)]
 struct TurnRadius(Length);
-#[derive(Debug)]
-struct Curvature(InverseMeter);
 
 impl TurnRadius {
     fn new<T>(value: V) -> Self
-    where T: uom::si::length::Unit + uom::Conversion<V, T = V>
+    where
+        T: uom::si::length::Unit + uom::Conversion<V, T = V>,
     {
         TurnRadius(Length::new::<T>(value))
     }
@@ -31,6 +31,29 @@ impl TurnRadius {
     pub fn to_curvature(&self) -> Curvature {
         Curvature(1.0 / self.0)
     }
+
+    pub fn zero() -> Self {
+        Self(Length::zero())
+    }
+
+    pub fn inf<T>() -> Self
+    where
+        T: uom::si::length::Unit + uom::Conversion<V, T = V>,
+    {
+        Self(Length::new::<T>(V::INFINITY))
+    }
+}
+
+#[derive(Debug)]
+struct Curvature(InverseMeter);
+
+impl Curvature {
+    pub fn zero<T>() -> Self
+    where
+        T: uom::si::length::Unit + uom::Conversion<V, T = V>,
+    {
+        TurnRadius::inf::<T>().to_curvature()
+    }
 }
 
 fn main() {
@@ -39,14 +62,27 @@ fn main() {
     let curvature0: InverseMeter = 1.0 / turn_radius0;
     println!("turn radius {turn_radius0:?} -> curvature {curvature0:?}");
 
-    let turn_radius1 = TurnRadius::new::<meter>(10.0);
+    let _turn_radius1 = TurnRadius::new::<meter>(10.0);
     // let curvature: Curvature = 1.0 / turn_radius;
     // these fail to compile as desired
     // turn_radius1 = turn_radius0;
     // println!("turn radius {turn_radius1:?} ->  {}", turn_radius0 == turn_radius1);
 
-    // but can't yet 1.0 / turn_radius without Div impl above
-    let curvature1: Curvature = turn_radius1.to_curvature();
-    let turn_radius1b = TurnRadius::from_curvature(&curvature1);
-    println!("turn radius {turn_radius1:?} -> curvature {curvature1:?} -> {turn_radius1b:?}");
+    for radius in [10.0, -10.0, 0.0, f64::INFINITY, f64::NEG_INFINITY] {
+        let turn_radius2 = TurnRadius::new::<meter>(radius);
+        let curvature2: Curvature = turn_radius2.to_curvature();
+        let turn_radius2b = TurnRadius::from_curvature(&curvature2);
+        println!("turn radius {turn_radius2:?} -> curvature {curvature2:?} -> {turn_radius2b:?}");
+    }
+
+    let turn_radius = TurnRadius::zero();
+    println!("turn_radius {turn_radius:?}");
+    // TODO(lucasw) it seems like meter shouldn't be required
+    let turn_radius = TurnRadius::inf::<meter>();
+    println!("turn_radius {turn_radius:?}");
+
+    let curvature = Curvature::zero::<meter>();
+    println!("curvature {curvature:?}");
+    // TODO(lucasw) want to have inverse_meter
+    // let curvature = Curvature::new::<inverse_meter>(0.0);
 }
